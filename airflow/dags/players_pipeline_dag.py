32528658a766d8d46db6a1da4f8c09cadafffa48
Luis Cortes <luiscs.dev@gmail.com>
2024-07-01 01:27:44 -0600

    add dag for ingest players

--- airflow/dags/players_pipeline_dag.py
+++ airflow/dags/players_pipeline_dag.py
@@ -25,6 +25,10 @@ from airflow.decorators import dag, task
 from pendulum import datetime
 import requests
 from airflow.operators.python import PythonOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
 
 
 def post_to_cloud_function(**kwargs):   
@@ -56,9 +60,57 @@ def post_to_cloud_function(**kwargs):
     tags=["players"],
 )
 def players_pipeline():
-    call_cloud_function = PythonOperator(
-        task_id='call_cloud_function',
-        python_callable=post_to_cloud_function,
-    )
+    
+    @task
+    def ingest_gsheet(payload: dict) -> None:
+            headers = {
+                'Content-Type': 'application/json'
+            }             
+            # Make the POST request
+            response = requests.post("https://us-central1-data-eng-training-87b25bc6.cloudfunctions.net/apc-data-ingestion", headers=headers, json=payload)
+            
+            # Check the response
+            if response.status_code != 200:
+                raise ValueError(f"Request to Cloud Function failed: {response.status_code} {response.text}")
 
+
+    ingest_provider1 = ingest_gsheet({
+        "spreadsheet_id": "https://docs.google.com/spreadsheets/d/1BNS_pZVMt55wlqkv2qfZiW8LPdcefFHPoYRFY4WVjf8",
+        "tab_name": "players",
+        "bucket_name": "apc-data-lake",
+        "file_name": "raw/airflow1.csv"
+    })
+    
+    ingest_provider2 = ingest_gsheet({
+        "spreadsheet_id": "https://docs.google.com/spreadsheets/d/1wNEh5U0YRerx2z_NXHdwpSgeFTXWtahakqw7iDWNQxo",
+        "tab_name": "players",
+        "bucket_name": "apc-data-lake",
+        "file_name": "raw/airflow2.csv"
+    })
+    
+    ingest_provider3 = ingest_gsheet({
+        "spreadsheet_id": "https://docs.google.com/spreadsheets/d/1ewdBpIk8tJGZrUdnUUQPyyiFoECuBqm8qPonlw37Hp0",
+        "tab_name": "players",
+        "bucket_name": "apc-data-lake",
+        "file_name": "raw/airflow3.csv"
+    })
+    
+
+    provider1_tobq = aql.load_file(
+        task_id='provider1_tobq',
+        input_file=File(
+            'gs://apc-data-lake/raw/airflow1.csv',
+            conn_id='google_cloud_con',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_players1',
+            conn_id='gcp',
+            metadata=Metadata(schema='apc_dwh')
+        ),
+        use_native_support=False,
+    )
+    
+    [ingest_provider1, ingest_provider2, ingest_provider3] >> provider1_tobq
+    
 players_pipeline()
